081a21fd8a7e1807b54f68cde7b23ab731305bc4
jamesole <87213195+jamesole@users.noreply.github.com>
2022-12-16 12:07:19 -0500

    Create errorResponse.js

---
+++ server/utils/errorResponse.js
@@ -0,0 +1,10 @@
+//Blue Print for ErrorResponse message 
+
+class ErrorResponse extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+module.exports = ErrorResponse;
